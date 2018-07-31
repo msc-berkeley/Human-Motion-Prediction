6debd7f3d271434050b4f62774fe09fb0ca74b51
CaesarAndylaw <caesarzwy@gmail.com>
2018-07-31 19:18:04 +0800

    Add files via upload

---
+++ lib/modi_order.m
@@ -0,0 +1,12 @@
+%% modify error order to original: 
+function error_ori = modi_order(error)
+ num_order = size(error,1);
+    error_ori = zeros(num_order, 9);
+    j = 1;
+    for start_order = 1:3
+        for i = start_order:3:num_order
+         error_ori(i,:) = error(j,:);
+         j = j + 1;
+        end
+    end
+end
